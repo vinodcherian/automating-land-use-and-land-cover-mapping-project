679113b3ab1540c236586e00628cea3c8154a46d
Vinod Cherian <vinodcherian5@gmail.com>
2023-05-06 20:17:21 +0530

    Update 2_Download_Files.py

--- pages/2_Download_Files.py
+++ pages/2_Download_Files.py
@@ -1,19 +1,26 @@
 
 import streamlit as st
+from project_utils.page_layout_helper import  main_header
 #from project_utils.azure_blob_storage_helper import get_file_gdrive, fetch_model, gdal_uploaded_image_array, model_result, image_to_rgb, predicted_image_with_class_label
 
-option = st.selectbox(
+
+def main():
+  main_header()
+  with st.container():
+    option = st.selectbox(
     'Please select the ROI region option to download the files : ',
     ('Full Camerron region','Adamawa','Central','East','Far North','Littoral','North','Northwest','South','Southwest','West'))
+    st.write('You selected:', option)
+    if option == 'Full Camerron region':
+        pass
+    elif option in ['Adamawa','Central','East','Far North','Littoral','North','Northwest','South','Southwest','West']:
+        pass
+    else:
+        st.write("Please select a valid option")
 
-st.write('You selected:', option)
-
-if option == 'Full Camerron region':
-  pass
-elif option in ['Adamawa','Central','East','Far North','Littoral','North','Northwest','South','Southwest','West']:
-  pass
-else:
-  st.write("Please select a valid option")
+    
+if __name__ == "__main__":
+  main()  
 
 #def get_full_country_data():
 #  return get_full_country_storage_blob()
