2d4d1c3b44077de1b760ee9a56f613506627ff29
Vinod Cherian <vinodcherian5@gmail.com>
2023-05-06 17:29:08 +0530

    Update 2_Download_Files.py

--- pages/2_Download_Files.py
+++ pages/2_Download_Files.py
@@ -1,6 +1,7 @@
 
 import streamlit as st
-from project_utils.azure_blob_storage_helper import 
+from project_utils.azure_blob_storage_helper
+
 option = st.selectbox(
     'Please select the ROI region option to download the files : ',
     ('Full Camerron region','Adamawa','Central','East','Far North','Littoral','North','Northwest','South','Southwest','West'))
@@ -14,18 +15,19 @@ elif option in ['Adamawa','Central','East','Far North','Littoral','North','North
 else:
   st.write("Please select a valid option")
 
-def get_full_country_data():
-  return get_full_country_storage_blob()
-
-def list_all_files(province):
-  return 
-
-def get_provinces_data():
-  return  
-def get_full_country_storage_blob():
-  return 
-
-def list_all_storage_blobs():
-  return 
-
-def get_provinces_storage_blob():
+#def get_full_country_data():
+#  return get_full_country_storage_blob()
+#
+#def list_all_files(province):
+#  return 
+#
+#def get_provinces_data():
+#  return  
+#def get_full_country_storage_blob():
+#  return 
+#
+#def list_all_storage_blobs():
+#  return 
+#
+#def get_provinces_storage_blob():
+#
