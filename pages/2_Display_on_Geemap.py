4024c6da71902793838a99c6ea7603b99d452a15
Vinod Cherian <vinodcherian5@gmail.com>
2023-05-12 23:13:52 +0530

    Update 2_Display_on_Geemap.py

--- pages/2_Display_on_Geemap.py
+++ pages/2_Display_on_Geemap.py
@@ -61,16 +61,17 @@ import streamlit as st
 import geemap.foliumap as geemap
 import geopandas as gpd
 
+AOI_GEOJSON= st.secrets["cameroon_aoi_bbx"] 
+predicted_image_asset_path=st.secrets("predicted_image_asset")
+json_data = st.secrets["google_map_auth_json_data"]
 json_object = json.loads(json_data, strict=False)
-service_account = json_object['client_email']
 json_object = json.dumps(json_object)
-
 os.environ['EARTHENGINE_TOKEN']=json_object
 geemap.ee_initialize(token_name='EARTHENGINE_TOKEN', auth_mode='notebook', service_account=True)
 m = geemap.Map(center=(7.3696, 12.3446), zoom=6)
-image=ee.Image("users/roygeoai/OMDENACMRLC2021")
+image=ee.Image(PREDICTED_IMAGE_ASSET_PATH)
 #image=ee.Image("https://code.earthengine.google.com/?asset=users/roygeoai/OMDENACMRLC2021")
-databox = gpd.read_file(os.path.abspath('./dla_aoi_bbx_200_m.geojson'))
+databox = gpd.read_file(os.path.abspath(AOI_GEOJSON))
 aoi = geemap.geopandas_to_ee(databox).geometry()
 m.centerObject(aoi,6)
 m.addLayer(image)
