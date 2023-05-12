f8b84e7e20c1779ee80e98a8d04e00b250835908
Vinod Cherian <vinodcherian5@gmail.com>
2023-05-12 23:02:59 +0530

    Update 2_Display_on_Geemap.py

--- pages/2_Display_on_Geemap.py
+++ pages/2_Display_on_Geemap.py
@@ -6,11 +6,11 @@
 #import geemap
 #import src.gee as gee
 
-import streamlit as st
-from streamlit_folium import folium_static
-import folium
-import ee
-import geemap.eefolium as geemap
+#import streamlit as st
+#from streamlit_folium import folium_static
+#import folium
+#import ee
+#import geemap.eefolium as geemap
 
 # Data from the downloaded JSON file
 #json_data = '''
@@ -27,28 +27,54 @@ import geemap.eefolium as geemap
 #   "client_x509_cert_url":"..."
 #}
 #''
-service_account= '@'
-predicted_image_asset="users/roygeoai/OMDENACMRLC2021"
+#service_account= '@'
+#predicted_image_asset="users/roygeoai/OMDENACMRLC2021"
+#
+#
+#AOI_GEOJSON= st.secrets["cameroon_aoi_bbx"] #'./dla_aoi_bbx_200_m.geojson'
+#predicted_image_asset_path=st.secrets("predicted_image_asset")
+#json_data = st.secrets["json_data"]
+#service_account = st.secrets["service_account"]
+#
+#
+#json_object = json.loads(json_data, strict=False)
+#json_object = json.dumps(json_object)
+#credentials = ee.ServiceAccountCredentials(service_account, key_data=json_object)
+##ee.Initialize(credentials)
+#geemap.ee_initialize()
+#image=ee.Image()
+##image=ee.Image("https://code.earthengine.google.com/?asset=users/roygeoai/OMDENACMRLC2021")
+#databox = gpd.read_file(AOI_GEOJSON)
+#aoi = geemap.geopandas_to_ee(databox).geometry()
+#m = geemap.Map()
+#m.centerObject(aoi,7)
+#m.addLayer(image)
+#m.addLayerControl()
+## call to render Folium map in Streamlit
+#folium_static(m)
 
-
-AOI_GEOJSON= st.secrets["cameroon_aoi_bbx"] #'./dla_aoi_bbx_200_m.geojson'
-predicted_image_asset_path=st.secrets("predicted_image_asset")
-json_data = st.secrets["json_data"]
-service_account = st.secrets["service_account"]
-
+import ee
+import os
+#import geemap
+import json
+import streamlit as st
+import geemap.foliumap as geemap
+import geopandas as gpd
 
 json_object = json.loads(json_data, strict=False)
+service_account = json_object['client_email']
 json_object = json.dumps(json_object)
-credentials = ee.ServiceAccountCredentials(service_account, key_data=json_object)
-#ee.Initialize(credentials)
-geemap.ee_initialize()
-image=ee.Image()
+
+os.environ['EARTHENGINE_TOKEN']=json_object
+geemap.ee_initialize(token_name='EARTHENGINE_TOKEN', auth_mode='notebook', service_account=True)
+m = geemap.Map(center=(7.3696, 12.3446), zoom=6)
+image=ee.Image("users/roygeoai/OMDENACMRLC2021")
 #image=ee.Image("https://code.earthengine.google.com/?asset=users/roygeoai/OMDENACMRLC2021")
-databox = gpd.read_file(AOI_GEOJSON)
+databox = gpd.read_file(os.path.abspath('./dla_aoi_bbx_200_m.geojson'))
 aoi = geemap.geopandas_to_ee(databox).geometry()
-m = geemap.Map()
-m.centerObject(aoi,7)
+m.centerObject(aoi,6)
 m.addLayer(image)
 m.addLayerControl()
-# call to render Folium map in Streamlit
-folium_static(m)
+m.to_streamlit()
+
+
